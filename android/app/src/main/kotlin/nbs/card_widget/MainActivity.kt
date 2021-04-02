560fb36d82dc0ac767bcbfa2af5956a5bf4a6a47
nofalbriansah <nofalbriansah@gmail.com>
2021-04-02 16:12:07 +0700

    extract method

---
+++ android/app/src/main/kotlin/nbs/card_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package nbs.card_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.green,
+        body: Container(
+          margin: EdgeInsets.all(10),
+          child: ListView(
+            children: [
+              buildCard(Icons.account_box, "Acount Box"),
+              buildCard(Icons.adb, "Android")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Card buildCard(IconData iconData, String text) {
+    return Card(
+      elevation: 5,
+      child: Row(
+        children: [
+          Container(
+              margin: EdgeInsets.all(5),
+              child: Icon(
+                iconData,
+                color: Colors.green,
+              )),
+          Text(text)
+        ],
+      ),
+    );
+  }
+}
